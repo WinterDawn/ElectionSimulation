3fa38165819be6e8b64600093d77829a7ac74842
dowu6943 <dowu6943@colorado.edu>
2019-03-06 00:54:53 -0700

    initiation

--- Election.h
+++ Election.h
@@ -1,9 +1,49 @@
 #ifndef _ELECTION_H_
 #define _ELECTION_H_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
 /** 
 
 */
+enum class Party {
+    None,
+    CPC,
+    RCCK,
+    CDL, 
+};
 
+struct Candidate {
+    string names;
+    Party affiliation;
+    int id;
+};
+
+class Election {
+    public:
+        Election();
+
+        vector<Candidate> get_candidates() const { return candidates_; }
+
+        void registration();
+        void campaign();
+        void vote();
+        void report();
+
+    protected:
+        vector<Candidate> candidates_;
+};
+
+class RepresentativeElection : public Election {
+
+};
+
+class DirectElection : public Election {
+
+};
 
 
 
--- ElectoralMap.h
+++ ElectoralMap.h
@@ -1,11 +1,25 @@
 #ifndef _ELECTORAL_MAP_H_
 #define _ELECTORAL_MAP_H_
 
+#include "District.h"
 /** 
 
 */
 
+class ElectoralMap {
+    public:
 
+        static ElectoralMap& GetInstance() {
+            static ElectoralMap instance; // guaranteed to be destroyed
+            return instance;
+        }
+
+        ElectoralMap(ElectoralMap const&) = delete; // copy constructor
+        void operator=(ElectoralMap const&) = delete; // assignment operator
+
+    private:
+        ElectoralMap(); 
+};  
 
 
 #endif  // _ELECTORAL_MAP_H_
